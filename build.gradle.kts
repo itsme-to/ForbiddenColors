239fc5a786e0930ba38cdcaa498f820f9b85327d
Théo Dufrene <43671109+itsme-to@users.noreply.github.com>
2025-11-13 16:52:15 +0100

    Migrate project from Maven to Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+}
+
+group = "to.itsme"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    // équivalent de <scope>provided</scope>
+    compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
